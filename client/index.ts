7bf7126cf91462916d83ee906f83724c062f176e
rudransh <rudransh9shinghal@gmail.com>
2024-06-03 15:47:24 +0530

    done

---
+++ client/index.ts
@@ -0,0 +1,57 @@
+import { CosmWasmClient , SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { SigningStargateClient, StargateClient , GasPrice  } from "@cosmjs/stargate";
+const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
+const { assertIsBroadcastTxSuccess } = require("@cosmjs/stargate");
+
+const mnemonic = ""
+
+
+async function connect() {
+
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: "mantra" });
+    const [{ address }] = await wallet.getAccounts();
+
+    const rpcEndpoint = "https://rpc.hongbai.mantrachain.io"
+
+    const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
+
+    console.log("Connected to node", await client.getChainId())
+    console.log("Account address:", address)
+
+    return {client , address}
+}
+
+
+export async function getCosmWasmClient(
+    rpcEndpoint: string,
+    mnemonic: string,
+    prefix: string
+  ): Promise<CosmWasmClient> {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix });
+    const [account] = await wallet.getAccounts();
+    const client = await CosmWasmClient.connect(rpcEndpoint)
+    return client ; 
+  }
+
+
+  async function getSigningCosmWasmClient(
+    rpcEndpoint: string,
+    mnemonic: string,
+    prefix: string
+  ): Promise<SigningCosmWasmClient> {
+    // Create a wallet from the mnemonic
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix });
+    const [account] = await wallet.getAccounts();
+    
+    // Connect to the blockchain with the signer
+    const client = await SigningCosmWasmClient.connectWithSigner(
+      rpcEndpoint,
+      wallet,
+      {
+        gasPrice: GasPrice.fromString('0.0025uom'),
+      }
+    );
+
+  
+    return client;
+  }
