c8e5c0df0fe665f90ec584f054638610d60de2fd
rudransh <rudransh9shinghal@gmail.com>
2024-06-03 15:43:39 +0530

    done

--- deployer/index.ts
+++ deployer/index.ts
@@ -2,22 +2,24 @@ const { DirectSecp256k1HdWallet } = require('@cosmjs/proto-signing');
 const { assertIsBroadcastTxSuccess, SigningCosmWasmClient, CosmWasmClient  } = require('@cosmjs/cosmwasm-stargate');
 const { coins , GasPrice } = require('@cosmjs/stargate');
 const fs = require('fs');
+require('dotenv').config();
 
 
-const mnemonic = "bracket bone submit eight law vast donate clay erase elephant acid real seminar south educate monster category fabric example globe dragon manual brain enrich"; // Replace with your mnemonic
-const rpcEndpoint = "https://rpc.hongbai.mantrachain.io"; // Replace with your RPC endpoint
+
+const mnemonic = process.env.MNEMONIC; // Replace with your mnemonic
+const rpcEndpoint = process.env.RPC; // Replace with your RPC endpoint
 const contractWasmPath = "./contract.wasm"; // Path to your compiled contract
 
 async function deploy() {
   // Step 1: Set up wallet and client
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
-    prefix: "mantra", // Replace with the correct prefix for your chain
+    prefix: "orai", // Replace with the correct prefix for your chain
   });
   const [account] = await wallet.getAccounts();
   console.log(`Wallet address: ${account.address}`);
 
   // Step 2: Connect to the blockchain
-  const client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet , { gasPrice: GasPrice.fromString('0.0025uom')});
+  const client = await SigningCosmWasmClient.connectWithSigner(rpcEndpoint, wallet , { gasPrice: GasPrice.fromString('0.0025uorai')});
   console.log("Connected to blockchain");
 
   // Step 3: Upload contract 
