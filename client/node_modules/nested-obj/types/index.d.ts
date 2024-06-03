dc2de81af3eb6ee2c5fed363e03ac63dea9abd5c
rudransh <rudransh9shinghal@gmail.com>
2024-06-03 15:32:58 +0530

    done

--- client/node_modules/nested-obj/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: {
-    get<T>(obj: Record<string, any>, path: string): T | undefined;
-    set(obj: Record<string, any>, path: string, value: any): void;
-    has(obj: Record<string, any>, path: string): boolean;
-};
-export default _default;
